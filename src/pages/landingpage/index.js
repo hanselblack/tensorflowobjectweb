0e30990a8aefcb62d63a5188b6b6743fa7697cf8
Hansel Ng <eng.sheng.ng@accenture.com>
2019-07-01 01:12:40 +0800

    initial commit

--- src/pages/landingpage/index.js
+++ src/pages/landingpage/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
     return (
-        <div>
-            <h1>test</h1>
+        <div className="container">
+            <Grid className="landing-grid">
+                <Grid item xs={12}>
+                <h1>test</h1>
+                </Grid>
+            </Grid>
         </div>
     );
 }
